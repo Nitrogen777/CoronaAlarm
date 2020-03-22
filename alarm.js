1e8d224debd6a7c0a0157ead8f5a238d09d254b2
EyShap <eyalm1000@gmail.com>
2020-03-22 16:25:28 +0200

    -Inital

---
+++ alarm.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const axios = require('axios');
+const cheerio = require('cheerio');
+const app = express()
+const port = 3040
+
+app.get('/corona', async function(req, res){
+    var result = await axios.get("https://www.worldometers.info/coronavirus/");
+    var $ = cheerio.load(result.data)
+    let israel;
+    $("#main_table_countries_today > tbody:nth-child(2) > tr:nth-child(25) > td:nth-child(1)").each((i, elm) =>{
+        if($(elm).text() === "Israel"){
+            israel = $(elm).parent();
+        }
+    })
+    resjs = {sick: $("#maincounter-wrap > div > span").eq(0).text().replace("        ", ""),
+            recover: $("#maincounter-wrap > div > span").eq(2).text(),
+            isick: $(israel).find("td").eq(1).text(),
+            ireco: $(israel).find("td").eq(5).text()}
+    res.send(resjs);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
