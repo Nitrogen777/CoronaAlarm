eaa057d9076f8bfb8ee259913c200a9d76a46a39
Nitrogen777 <eyalm1000@gmail.com>
2020-03-23 15:12:45 +0200

    -No longer relying on Israels position in the table

--- alarm.js
+++ alarm.js
@@ -8,9 +8,10 @@ app.get('/corona', async function(req, res){
     var result = await axios.get("https://www.worldometers.info/coronavirus/");
     var $ = cheerio.load(result.data)
     let israel;
-    $("#main_table_countries_today > tbody:nth-child(2) > tr:nth-child(25) > td:nth-child(1)").each((i, elm) =>{
+    $("#main_table_countries_today > tbody > tr > td:nth-child(1)").each((i, elm) =>{
         if($(elm).text() === "Israel"){
             israel = $(elm).parent();
+            
         }
     })
     resjs = {sick: $("#maincounter-wrap > div > span").eq(0).text().replace("        ", ""),
