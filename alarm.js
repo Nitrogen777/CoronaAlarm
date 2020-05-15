b0382eea3211fd66d650844775e17d77abe26e18
EyShap <eyalm1000@gmail.com>
2020-05-15 09:17:35 +0300

    -Adjusted to changes on the site

--- alarm.js
+++ alarm.js
@@ -15,38 +15,38 @@ async function getTotal(result){
 async function getCountryInfo(countryName, result){
     var $ = cheerio.load(result.data)
     let info;
-    $("#main_table_countries_today > tbody > tr > td:nth-child(1)").each((i, elm) =>{
+    $("#main_table_countries_today > tbody:nth-child(2) > tr > td:nth-child(2) > a").each((i, elm) =>{
         if($(elm).text().toLowerCase() === countryName.toLowerCase()){
-            info = $(elm).parent();
+            info = $(elm).parent().parent();
         }
         
     })
     
     let response = {
-        totali: $(info).find("td").eq(1).text().replace(/ +/, ""),
-        newi: $(info).find("td").eq(2).text().replace(/ +/, ""),
-        totald: $(info).find("td").eq(3).text().replace(/ +/, ""),
-        newd: $(info).find("td").eq(4).text().replace(/ +/, ""),
-        recover: $(info).find("td").eq(5).text().replace(/ +/, ""),
-        active: $(info).find("td").eq(6).text().replace(/ +/, ""),
-        critical: $(info).find("td").eq(7).text().replace(/ +/, "")
+        totali: $(info).find("td").eq(2).text().replace(/ +/, ""),
+        newi: $(info).find("td").eq(3).text().replace(/ +/, ""),
+        totald: $(info).find("td").eq(4).text().replace(/ +/, ""),
+        newd: $(info).find("td").eq(5).text().replace(/ +/, ""),
+        recover: $(info).find("td").eq(6).text().replace(/ +/, ""),
+        active: $(info).find("td").eq(7).text().replace(/ +/, ""),
+        critical: $(info).find("td").eq(8).text().replace(/ +/, "")
     };
     return response;
 }
 async function getAll(result){
     var $ = cheerio.load(result.data)
     let arr = [];
-    $("#main_table_countries_today > tbody > tr > td:nth-child(1)").each((i, elm) =>{
-        let info = $(elm).parent();
+    $("#main_table_countries_today > tbody:nth-child(2) > tr > td:nth-child(2) > a").each((i, elm) =>{
+        let info = $(elm).parent().parent();
         let response = {
-            name: $(info).find("td").eq(0).text().replace(/ +/, ""),
-            totali: $(info).find("td").eq(1).text().replace(/ +/, ""),
-            newi: $(info).find("td").eq(2).text().replace(/ +/, ""),
-            totald: $(info).find("td").eq(3).text().replace(/ +/, ""),
-            newd: $(info).find("td").eq(4).text().replace(/ +/, ""),
-            recover: $(info).find("td").eq(5).text().replace(/ +/, ""),
-            active: $(info).find("td").eq(6).text().replace(/ +/, ""),
-            critical: $(info).find("td").eq(7).text().replace(/ +/, "")
+            name: $(info).find("td").eq(1).find("a").text().replace(/ +/, ""),
+            totali: $(info).find("td").eq(2).text().replace(/ +/, ""),
+            newi: $(info).find("td").eq(3).text().replace(/ +/, ""),
+            totald: $(info).find("td").eq(4).text().replace(/ +/, ""),
+            newd: $(info).find("td").eq(5).text().replace(/ +/, ""),
+            recover: $(info).find("td").eq(6).text().replace(/ +/, ""),
+            active: $(info).find("td").eq(7).text().replace(/ +/, ""),
+            critical: $(info).find("td").eq(8).text().replace(/ +/, "")
         };
         arr.push(response);
     })
